38a07075dd52201d19f1ec95b337aab15ba8ad3d
hakmad <88987013+hakmad@users.noreply.github.com>
2022-08-14 12:48:26 +0100

    Added meta extension to md_reader
    The meta extension is pretty useful - it allows me to specify things
    like the title and date of a post in the Markdown file without that data
    appearing in the actual text. A post will look something like this:
    
    ```
    title: [post title]
    date: [post date]
    
    [actual content]
    ```
    
    The only issue is that the converter is simple - it assumes that all
    files will contain the title and date metadata (not all posts will have
    the date metadata).

--- daisy.py
+++ daisy.py
@@ -5,6 +5,9 @@ class Post:
         self.filename = filename[:-3]
 
         with open(filename, "r") as file:
-            md_reader = markdown.Markdown()
+            md_reader = markdown.Markdown(extensions=["meta"])
 
             self.content = md_reader.convert(file.read())
+
+            self.title = md_reader.Meta["title"].pop()
+            self.date = md_reader.Meta["date"].pop()
