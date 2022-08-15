0a1c01512e1bedaea7107f872a04ce1e16f35df5
hakmad <88987013+hakmad@users.noreply.github.com>
2022-08-15 22:22:24 +0100

    Added docstrings to the Post class
    I've tried to be as concise and clear as possible. Whether that is the
    case or not is for whoever reads this code later (probably me in a few
    months time) to decide.

--- daisy.py
+++ daisy.py
@@ -2,7 +2,27 @@ import markdown
 import jinja2
 
 class Post:
+    """Class to represent a single post.
+
+    Attributes:
+    filename (str) - filename of the post (not including the extension).
+    content (str) - content of the post as a HTML fragment.
+    html (str) - fully rendered HTML of the post.
+    title (str) - title of the post.
+    date (str) - date of the post (in the ISO 8601 format).
+    """
     def __init__(self, filename):
+        """Setup the Post object.
+
+        Opens the file containing the post content and sets up the Post
+        object with the relevant attributes.
+
+        Arguments:
+        filename (str) - filename of the post to open.
+
+        Returns:
+        None
+        """
         self.filename = filename[:-3]
 
         with open(filename, "r") as file:
@@ -18,6 +38,15 @@ class Post:
                 self.date = None
 
     def render_html(self, post_type):
+        """Render the partial HTML to a full HTML page and write it
+        out to a file.
+
+        Arguments:
+        post_type (str) - type of template to use.
+
+        Returns:
+        None
+        """
         if post_type == "meta":
             template_file = "templates/meta.html"
         elif post_type == "blog":
